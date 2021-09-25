8553b74d3e58c576cae6198f12bd26729f475207
srbdev <sylvain.rb@gmail.com>
2021-09-25 00:44:02 +0000

    WIP: commit before shutting down instance

--- db/crud.py
+++ db/crud.py
@@ -11,5 +11,77 @@ def get_budgets(db: Session):
     return db.query(models.Budget).all()
 
 
-def create_budget(db: Session, budget: schemas.Budget):
+def create_budget(db: Session, budget: schemas.BudgetCreate):
+    db_budget = models.Budget(uuid=budget.uuid, name=budget.name)
+    db.add(db_budget)
+    db.commit()
+    db.refresh(db_budget)
+    return db_budget
+
+
+def delete_budget(db: Session, uuid: str):
+    pass
+
+
+def get_account(db: Session, uuid: str):
+    return db.query(models.Account).filter(models.Account.uuid == uuid).first()
+
+
+def get_accounts(db: Session):
+    return db.query(models.Account).all()
+
+
+def create_account(db: Session, account: schemas.Account):
+    pass
+
+
+def delete_account(db: Session, uuid: str):
+    pass
+
+
+def get_master_category(db: Session, uuid: str):
+    return db.query(models.MasterCategory).filter(models.MasterCategory.uuid == uuid).first()
+
+
+def get_master_categories(db: Session):
+    return db.query(models.MasterCategory).all()
+
+
+def create_master_category(db: Session, master_category: schemas.MasterCategory):
+    pass
+
+
+def delete_master_category(db: Session, uuid: str):
+    pass
+
+
+def get_category(db: Session, uuid: str):
+    return db.query(models.Category).filter(models.Category.uuid == uuid).first()
+
+
+def get_categories(db: Session):
+    return db.query(models.Category).all()
+
+
+def create_category(db: Session, category: schemas.Category):
+    pass
+
+
+def delete_category(db: Session, uuid: str):
+    pass
+
+
+def get_transaction(db: Session, uuid: str):
+    return db.query(models.Transaction).filter(models.Transaction.uuid == uuid).first()
+
+
+def get_transactions(db: Session):
+    return db.query(models.Transaction).all()
+
+
+def create_transaction(db: Session, category: schemas.Transaction):
+    pass
+
+
+def delete_transaction(db: Session, uuid: str):
     pass
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI, Request
+from typing import List
+
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm.session import Session
+
+from db import crud, models, schemas
+from db.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/status")
@@ -18,7 +33,8 @@ async def status():
 # Budget Model ***************************************************************#
 
 @app.get("/budgets", response_class=HTMLResponse)
-async def budget_index(request: Request):
+async def read_budgets(request: Request, db: Session = Depends(get_db)):
+    budgets = crud.get_budgets(db)
     return templates.TemplateResponse("budget/index.html", {"request": request})
 
 # Budget Model - END *********************************************************#
